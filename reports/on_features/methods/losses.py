6dc5e1109ed8eae5c724972f1a093d8db680581a
franchesoni <marchesoniacland@gmail.com>
2023-12-27 14:09:34 +0100

    Refactor loss functions and clean up code

--- reports/on_features/methods/losses.py
+++ reports/on_features/methods/losses.py
@@ -1,6 +1,7 @@
 import torch
 from functools import partial
 
+
 def preprocess_masks_features(masks, features):
     # Get shapes right
     B, M, H, W = masks.shape
@@ -24,15 +25,16 @@ def preprocess_masks_features(masks, features):
 
     return masks, features, M, B, H, W, F
 
+
 def symlog(x):
     return torch.sign(x) * torch.log(torch.abs(x) + 1)
+
 
 def get_row_col(H, W, device):
     # get position of pixels in [0, 1]
     row = torch.linspace(0, 1, H, device=device)
     col = torch.linspace(0, 1, W, device=device)
     return row, col
-    
 
 
 def simplest_loss(features, masks, alpha=1, reg_w=None):
@@ -59,13 +61,19 @@ def simplest_loss(features, masks, alpha=1, reg_w=None):
     ).sum(dim=2)
     push_loss = push_loss.sum(dim=1) / M  # mean over masks
 
-    reg_loss = 0 if reg_w is None else reg_w * torch.norm(mean_features, dim=2, p=1).sum(dim=1) / M  # mean over masks
+    reg_loss = (
+        0
+        if reg_w is None
+        else reg_w * torch.norm(mean_features, dim=2, p=1).sum(dim=1) / M
+    )  # mean over masks
     loss = alpha * pull_loss + push_loss + reg_loss
     loss = loss.mean()  # mean over batch
     return loss
 
 
-def simplest_hinge(features, masks, ball_radius=0.15, reg_w=None, use_push_centers=True):
+def simplest_hinge(
+    features, masks, ball_radius=0.15, reg_w=None, use_push_centers=True
+):
     """Pull features of one mask to its center if they're outside pull ball. Push features external to the mask away from the center if they're inside push ball. Push centers apart if they're contained in the same push centers ball."""
     if reg_w is None:
         features = torch.sigmoid(features)
@@ -99,7 +107,6 @@ def simplest_hinge(features, masks, ball_radius=0.15, reg_w=None, use_push_cente
     ).sum(dim=2) / (~masks.reshape(B, M, H * W)).sum(dim=2)
     push_loss = push_loss.sum(dim=1) / M  # mean over masks
 
-
     if use_push_centers:
         # push centers loss: push centers away from each other if they're inside the push centers ball and the masks don't overlap
         distinct_masks = (
@@ -109,7 +116,9 @@ def simplest_hinge(features, masks, ball_radius=0.15, reg_w=None, use_push_cente
         )  # B, M, M
         center_distances = torch.clamp(
             push_centers_ball_radius
-            - torch.cdist(mean_features.reshape(B, M, F), mean_features.reshape(B, M, F)),
+            - torch.cdist(
+                mean_features.reshape(B, M, F), mean_features.reshape(B, M, F)
+            ),
             min=0,
         )  # B, M, M, hinged
         push_centers_loss = (
@@ -127,10 +136,15 @@ def simplest_hinge(features, masks, ball_radius=0.15, reg_w=None, use_push_cente
 
     ## comment out regularization loss
 
-    reg_loss = 0 if reg_w is None else reg_w * torch.norm(mean_features, dim=2, p=1).sum(dim=1) / M  # mean over masks
+    reg_loss = (
+        0
+        if reg_w is None
+        else reg_w * torch.norm(mean_features, dim=2, p=1).sum(dim=1) / M
+    )  # mean over masks
     loss = pull_loss + push_loss + push_centers_loss + reg_loss
     loss = loss.mean()  # mean over batch
     return loss
+
 
 def offset_to_center(features, masks, reg_w=None):
     """Try to predict as feature the center of the mask"""
@@ -141,14 +155,12 @@ def offset_to_center(features, masks, reg_w=None):
     # masks: B, M, 1, H*W
     # everything will be: B, M, F, H*W
     # get position of pixels in [0, 1]
-    row, col = get_row_col(H, W, features.device)  
-    row = (row.reshape(1, 1, 1, H, 1)
-        .expand(B, 1, 1, H, W)
-        .reshape(B, 1, 1, H * W))
-    col = (col.reshape(1, 1, 1, 1, W)
-        .expand(B, 1, 1, H, W)
-        .reshape(B, 1, 1, H * W))
-    positional_features = torch.cat([row, col] + [torch.zeros_like(row)]*(max(0, F-2)), dim=2)  # B, 1, F, H*W
+    row, col = get_row_col(H, W, features.device)
+    row = row.reshape(1, 1, 1, H, 1).expand(B, 1, 1, H, W).reshape(B, 1, 1, H * W)
+    col = col.reshape(1, 1, 1, 1, W).expand(B, 1, 1, H, W).reshape(B, 1, 1, H * W)
+    positional_features = torch.cat(
+        [row, col] + [torch.zeros_like(row)] * (max(0, F - 2)), dim=2
+    )  # B, 1, F, H*W
 
     # now we can mask and compute target masks which are centers of masks
     target_features = positional_features * masks  # B, M, F, H*W
@@ -157,7 +169,7 @@ def offset_to_center(features, masks, reg_w=None):
     )  # B, M, F, 1  # mean over pixels
 
     # now we can compute the loss
-    preds = (positional_features - features)
+    preds = positional_features - features
     distances = torch.norm(preds - target_features, dim=2, p=1)  # B, M, H*W, distance
     loss = distances * masks.reshape(B, M, H * W)  # B, M, H*W, distance
     loss = loss.sum(dim=2) / masks.reshape(B, M, H * W).sum(dim=2)  # B, M, distance
@@ -167,13 +179,14 @@ def offset_to_center(features, masks, reg_w=None):
     loss = loss + reg_loss
     return loss
 
+
 def global_variance(features, masks):
     features = symlog(features)
     masks, features, M, B, H, W, F = preprocess_masks_features(masks, features)
-    loss = - torch.var(features, dim=(3)).mean()  # negative variance as loss increases variance hence diversity
+    loss = -torch.var(
+        features, dim=(3)
+    ).mean()  # negative variance as loss increases variance hence diversity
     return loss
-
-
 
 
 ##########################################################
@@ -307,38 +320,52 @@ def global_variance(features, masks):
 #                }
 
 
+losses_dict = {
+    "simplest": simplest_loss,
+    "hinge": simplest_hinge,
+    "offset": offset_to_center,
+    "global_var": global_variance,
+}
 
 
-losses_dict = {"simplest": simplest_loss,
-               "hinge": simplest_hinge,
-               "offset": offset_to_center,
-               "global_var": global_variance,}
-
-def try_loss(loss_name, runname=None, datadir='ofdata', sample_index=3, n_iter=1000, out_channels=3, loss_kwargs={}, clean=False):
+def try_loss(
+    loss_name,
+    runname=None,
+    datadir="ofdata",
+    sample_index=3,
+    n_iter=1000,
+    out_channels=3,
+    loss_kwargs={},
+    clean=False,
+):
     assert type(loss_kwargs) == dict, "loss_kwargs must be a dict, use quotes"
     from pathlib import Path
     import json
     from extras.utils import get_current_git_commit
+
     if runname is None:
         from haikunator import Haikunator
+
         runname = Haikunator().haikunate()
     import datetime
+
     runname = f"{datetime.datetime.now().strftime('%Y%m%d_%H%M%S')}_{runname}"
     if clean:
         clean_ascent()
 
-    dstdir = Path('ascent') / runname
+    dstdir = Path("ascent") / runname
     dstdir.mkdir(exist_ok=False, parents=True)
-    hparams = {'loss_name': loss_name,
-                'runname': runname,
-                'datadir': datadir,
-                'sample_index': sample_index,
-                'n_iter': n_iter,
-                'out_channels': out_channels,
-                'loss_kwargs': loss_kwargs,
-                'git_commit': get_current_git_commit(),
-                }
-    with open(dstdir / 'hparams.json', 'w') as f:
+    hparams = {
+        "loss_name": loss_name,
+        "runname": runname,
+        "datadir": datadir,
+        "sample_index": sample_index,
+        "n_iter": n_iter,
+        "out_channels": out_channels,
+        "loss_kwargs": loss_kwargs,
+        "git_commit": get_current_git_commit(),
+    }
+    with open(dstdir / "hparams.json", "w") as f:
         json.dump(hparams, f, indent=4)
 
     loss_fn = losses_dict[loss_name]
@@ -349,12 +376,14 @@ def try_loss(loss_name, runname=None, datadir='ofdata', sample_index=3, n_iter=1
     train_ds, _ = get_train_val_ds(datadir)
     train_batch = custom_collate([train_ds[sample_index]])
     image, masks = train_batch
-    save_tensor_as_image(dstdir / 'image.jpg', image[0], 0)
+    save_tensor_as_image(dstdir / "image.jpg", image[0], 0)
 
     # do gradient ascent from tensor
     output = torch.randn(size=(1, out_channels, 224, 224), requires_grad=True)
     optimizer = torch.optim.Adam([output], lr=1)
-    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=n_iter // 10, verbose=True)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+        optimizer, patience=n_iter // 10, verbose=True
+    )
 
     for i in range(n_iter):
         loss = loss_fn(output, masks, **loss_kwargs)
@@ -365,22 +394,26 @@ def try_loss(loss_name, runname=None, datadir='ofdata', sample_index=3, n_iter=1
         print(f"{i} / {n_iter}, loss: {loss.item():.4e}", end="\r")
 
         if i % (n_iter // 10) == 0:
-            save_tensor_as_image(dstdir / 'ascent.jpg', output[0], i)
+            save_tensor_as_image(dstdir / "ascent.jpg", output[0], i)
 
-    print('Done')
-    with open(dstdir / 'done.txt', 'w') as f:
-        f.write('Done')
+    print("Done")
+    with open(dstdir / "done.txt", "w") as f:
+        f.write("Done")
+
 
 def clean_ascent():
     import shutil
     import datetime
     from pathlib import Path
-    dstdir = Path('ascent')
+
+    dstdir = Path("ascent")
     for f in dstdir.iterdir():
         # if the directory doesn't have a done.txt file remove it
-        if f.is_dir() and not (f / 'done.txt').exists():
+        if f.is_dir() and not (f / "done.txt").exists():
             shutil.rmtree(f)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     from fire import Fire
+
     Fire(try_loss)
